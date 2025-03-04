73aa2f9ff62c11eb3e4fb581ae75634b533577b1
doomdex84 <doomdex84@gmail.com>
2025-03-04 14:17:57 +0900

    if문제

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,82 @@
 package org.example;
 
-public class Main{
-    public static void main(String[]args) {
-        int x = 10;
+// 문제 : 실행되는 출력문에는 참 그렇지 않으면 거짓 이라고 적어주세요.
 
-        System.out.println("x");
-        System.out.println(x);
-        System.out.println("x:" + x);
-        System.out.println("x:" + x * 10);
-        System.out.println("x:" + x + 10);
-        System.out.println("x:" + (x + 10));
+class Main {
+    public static void main(String[] args) {
+        if ( true ) {
+            System.out.println("참");
+        }
+
+        if ( false ) {
+            System.out.println("거짓");
+        }
+
+        int a = 10;
+
+        // `==` => 같다.
+        if ( a == 10 ) {
+            System.out.println("참1");
+        }
+
+        // `!=` => 같지 않다.
+        if ( a != 10 ) {
+            System.out.println("거짓1");
+        }
+
+        if ( a > 10 ) {
+            System.out.println("거짓2");
+        }
+
+        if ( a >= 10 ) {
+            System.out.println("참2");
+        }
+
+        int b = 10;
+
+        if ( a == b ) {
+            System.out.println("참3");
+        }
+
+        // boolean c => c 에는 오직 true/false 만 담을 수 있다.
+        boolean c = a != b;
+
+        // 이문제 다시 풀어보기
+
+        if ( c ) {
+            System.out.println("거짓3");
+        }
+
+        if ( c == false ) {
+            System.out.println("참4");
+        }
+
+
+
+        // `!` => 반전 / = not
+        if ( !c ) {
+            System.out.println("참5");
+        }
+
+
+        // `!` => 반전
+        if ( !(!c) ) {
+            System.out.println("참6");
+        }
+
+
+        boolean d = true;
+
+        if ( c != d ) {
+            System.out.println("참7");
+        }
+
+        if ( 20 > 2 && 10 > 3 && true != false && 10 != 10 ) {
+            System.out.println("거짓4");
+        }
+
+        if ( 10 != 10 || 10 < 2 ) {
+            System.out.println("거짓5");
+        }
     }
 }
