75248b3c2951ee3010547c990ced345733fd9335
doomdex84 <doomdex84@gmail.com>
2025-03-06 09:16:43 +0900

    0306

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,22 +3,32 @@
 //결과가 왜 이렇게 나오는가?를 서술
 //명확하게 한줄한줄 세부적으로 표현하기
 
-import java.util.Scanner;
+
 
 class Main {
     public static void main(String[] args) {
 
-        int a =97;
 
-        System.out.printf("%c\n",a);
-        System.out.printf("%d\n",a);
+        int n = 3;
 
-        System.out.println("정수 1개 입력해 : ");
-        Scanner sc = new Scanner(System.in); // System.in : 입력
-
-        int num = sc.nextInt();
-        System.out.println(num);
-
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--){
+            for (int k = n; k > i; k--){
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
 
