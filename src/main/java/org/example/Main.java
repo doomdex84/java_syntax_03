a6748ed244471e815be22dfb0635bc9c9acd6898
doomdex84 <doomdex84@gmail.com>
2025-03-12 12:57:03 +0900

    상속

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,44 +4,86 @@
 
 package org.example;
 
-import java.awt.*;
-import java.util.Arrays;
+// 상속
+
+/* 요구사항
+- D1 요구 : 오리를 생성하고 날게하시오.
+- D1 완료 : 클래스와 메서드를 만들어서 처리
+- D2 요구 : 게임의 디테일을 살리기 위해서 `청둥오리`와 `흰오리`를 생성하고 날게하시오.
+- D2 완료 : 클래스와 메서드를 만들어서 처리
+- D3 요구 : 오리 계열의 클래스에 날다 메서드가 중복되었습니다. 중복을 제거해주세요.
+- D3 완료 : 상속을 사용해서 처리
+- D4 요구 : 게임의 재미를 위하여 고무오리를 추가하고 날게 하시오.
+- D4 완료 : 클래스와 메서드를 만들어서 처리
+- D5 요구 : 주말 주주회의에서 고무오리가 하늘을 날아다니는 것에 대해서 태클이 들어왔습니다. 고무오리 계열은 하늘을 날 수 없게 해주세요.
+- D5 완료 : 메서드 오버라이드를 사용해서 처리
+- D6 요구 : 고무오리가 반응이 좋습니다. 고무2오리를 추가하고 날게 해주세요.
+- D6 완료 : 고무오리가 반응이 좋습니다. 고무2오리를 추가하고 날게 해주세요.
+- D7 요구 : 고무오리계열에서 날다 메서드의 중복을 제거해주세요.
+- D7 완료 : 상속으로 해결
+- D8 요구 : 자바에서 다중상속은 불가능 합니다. 상속에는 한계가 있습니다. 복잡한 상속을 사용하지 않고 고무오리계열의 중복을 제거해주세요.
+  - 복잡한 상속을 사용하지 않는다는 것의 의미 : 모든 오리 계열 클래스들은 부모클래스가 오리 클래서 여야 한다.
+*/
 
 class Main {
     public static void main(String[] args) {
+        청둥오리 a청둥오리 = new 청둥오리();
+        a청둥오리.날다();
+        // 출력 : 오리가 날개로 날아갑니다.
 
-        String msg = greet("Jin");
-        System.out.println(msg);
+        흰오리 a흰오리 = new 흰오리();
+        a흰오리.날다();
+        // 출력 : 오리가 날개로 날아갑니다.
 
-        // 문자열만 담을 수 있는 변수 msg에 greet 함수 실행 결과를 넣는다.
+        고무오리 a고무오리 = new 고무오리();
+        a고무오리.날다();
+        // 출력 : 저는 날 수 없어요. ㅜㅠ
 
-        Point point = createPoint(5, 10);
-        System.out.println("Point create at (" + point.x + ", " + point.y + ")");
-
-        // 포인트 모양의 객체와 연결 될 포인트 변수;
-
-        int[] evens = firstNEvenNumbers(5);
-        System.out.println("first 5 even numbers : " + Arrays.toString(evens));
+        고무2오리 a고무2오리 = new 고무2오리();
+        a고무2오리.날다();
+        // 출력 : 저는 날 수 없어요. ㅜㅠ
     }
-      // int[] 모양의 객체와 연결될 변수
+}
+
+class 오리 {
+    void 날다() {
+        System.out.println("오리가 날개로 날아갑니다.");
+
+            // 고무가 들어가면 못날도록 하겠다.도 있으면 어떻게 될까?
 
 
-
-
-    public static String greet(String name) { // 문자열 매개변수 1개 받는 greet(String) 메서드
-        return "Hello, " + name + "!"; // static ==  인스턴스화 없이 사용(new x)
     }
+}
 
-    public static Point createPoint(int x, int y) {
-        return new Point(x, y); 
+// 오리 => 부모 클래스
+// 흰오리 => 자식 클래스
+// 흰오리 extends 오리 => 흰오리가 오리의 능력을 물려 받는다.
+// "`흰오리`는 `오리`이다." 라고 자바에게 알려준다.
+// `A 는 B 이다.` 테스트에 통과하는 것만 상속이 가능하다.
+// 클래스와 객체는 명사이다.
+// 메서드는 동사이다.
+
+class 흰오리 extends 오리 {
+
+}
+
+class 청둥오리 extends 오리 {
+
+}
+
+class 고무오리 extends 오리 {
+
+    // 메서드 재정의 라고 합니다.
+    // 메서드 오버라이드 라고도 합니다.
+    // 메서드 재정의 => 부모가 물려준 능력을 다시 구현한다.
+    
+    void 날다() {
+        System.out.println("저는 날 수 없어요. ㅜㅠ");
     }
+}
 
-    public static int[] firstNEvenNumbers(int n) {
-        int[] evenNumbers = new int[n];
-
-        for (int i = 0, num = 2; i < n; i++, num += 2) {
-            evenNumbers[i] = num; //  evenNumber == 짝수
-        }
-        return evenNumbers;
+class 고무2오리 extends 오리 {
+    void 날다() {
+        System.out.println("저는 날 수 없어요. ㅜㅠ");
     }
 }
