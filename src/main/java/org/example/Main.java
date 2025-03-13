1c85be6203576eb7e7c62b56b65b41469adbc25d
doomdex84 <doomdex84@gmail.com>
2025-03-13 10:09:32 +0900

    Tv 리모컨

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,49 +3,67 @@ package org.example;
 // 자바자료형 boolean / char / byte / short / int / long /float / double
 //            논리값 / 문자 /             정수           /      실수    /
 
- //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// 문제 : 매개변수를 사용해서 전사가 매번 다르게 공격하도록 해주세요.
 
 class Main {
     public static void main(String[] args) {
-        전사 a전사 = new 전사();
+        샤오미Tv a샤오미Tv = new 샤오미Tv();
+        삼성Tv a삼성Tv = new 삼성Tv();
+        LGTv aLGTv = new LGTv();
 
-        a전사.공격("브라이언","칼");
-        // 브라이언이(가) 칼(으)로 공격합니다.
+        a샤오미Tv.켜기("샤오미");
+        // 출력 => 샤오미Tv 켜집니다.
+        a샤오미Tv.끄기("샤오미");
+        // 출력 => 샤오미Tv 꺼집니다.
+        a샤오미Tv.vr켜기("샤오미","vr");
+        // 출력 => 샤오미Tv vr켜기!
 
-        a전사.재공격();
-        // 브라이언이(가) 칼(으)로 공격합니다.
+        a삼성Tv.켜기("삼성");
+        // 출력 => 삼성Tv 켜집니다.
+        a삼성Tv.끄기("삼성");
+        // 출력 => 삼성Tv 꺼집니다.
+        a삼성Tv.ar켜기("삼성","ar");
+        // 출력 => 삼성Tv ar켜기!
 
-        a전사.공격("필립","창");
-        // 필립이(가) 창(으)로 공격합니다.
-
-        a전사.공격("마크","지팡이");
-        // 마크(가) 지팡이(으)로 공격합니다.
-
-        a전사.재공격();
-        // 마크(가) 지팡이(으)로 공격합니다.
-
-        a전사.재공격();
-        // 마크(가) 지팡이(으)로 공격합니다.
+        aLGTv.켜기("LG");
+        // 출력 => LGTv 켜집니다.
+        aLGTv.끄기("LG");
+        // 출력 => LGTv 꺼집니다.
+        aLGTv.게임모드전환("LG","게임모드");
+        // 출력 => LGTv 게임모드전환!
     }
 }
-class 전사 {
-    String lastName;
-    String lastWeaponName;
 
-    void 공격(String name, String weaponName) {
-        공격문구출력(name, weaponName);
+class tv{
 
-        lastName = name;
-        lastWeaponName = weaponName;
+   String lasta;
+   String lastb;
+
+    void 켜기(String a){
+        System.out.println( a + "Tv 켜집니다.");
+
+        String lasta;
+    }
+    void 끄기(String a){
+        System.out.println( a+ "Tv 꺼집니다.");
+        String lasta;
+    }
+}
+
+class 샤오미Tv extends tv{
+    void vr켜기(String a, String b ) {
+        System.out.println(a + "Tv " +  b +"켜기");
+    }
+}
+class 삼성Tv extends tv{
+    void ar켜기(String a, String b ) {
+        System.out.println(a + "Tv " + b + "켜기");
+    }
+}
+
+
+class LGTv extends tv{
+    void 게임모드전환(String a,String b ) {
+        System.out.println(a + "Tv " + b + "전환 ");
     }
 
-    void 공격문구출력(String name, String weaponName) {
-        System.out.println(name + "(이)가 " + weaponName + "(으)로 공격합니다.");
-    }
-
-    void 재공격() {
-        공격문구출력(lastName, lastWeaponName);
-    }
 }
