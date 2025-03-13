99aabd5712b434525c6fb2c23752a36163a05b5c
doomdex84 <doomdex84@gmail.com>
2025-03-13 15:08:41 +0900

    무기사용

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,66 +4,71 @@ package org.example;
 //            논리값 / 문자 /             정수           /      실수    /
 
 
-class Main {
+// 아래의 코드가 실행되도록 해주세요
+
+public class Main {
     public static void main(String[] args) {
-        샤오미Tv a샤오미Tv = new 샤오미Tv();
-        삼성Tv a삼성Tv = new 삼성Tv();
-        LGTv aLGTv = new LGTv();
+        전사 a전사 = new 전사();
 
-        a샤오미Tv.켜기("샤오미");
-        // 출력 => 샤오미Tv 켜집니다.
-        a샤오미Tv.끄기("샤오미");
-        // 출력 => 샤오미Tv 꺼집니다.
-        a샤오미Tv.vr켜기("샤오미","vr");
-        // 출력 => 샤오미Tv vr켜기!
+        String 이름 = "칸";
+        a전사.이름 = 이름;
+        a전사.나이 = 20;
+        a전사.자기소개();
 
-        a삼성Tv.켜기("삼성");
-        // 출력 => 삼성Tv 켜집니다.
-        a삼성Tv.끄기("삼성");
-        // 출력 => 삼성Tv 꺼집니다.
-        a삼성Tv.ar켜기("삼성","ar");
-        // 출력 => 삼성Tv ar켜기!
+        a전사.나이++;
+        a전사.자기소개();
 
-        aLGTv.켜기("LG");
-        // 출력 => LGTv 켜집니다.
-        aLGTv.끄기("LG");
-        // 출력 => LGTv 꺼집니다.
-        aLGTv.게임모드전환("LG","게임모드");
-        // 출력 => LGTv 게임모드전환!
+        a전사.나이 = 30;
+        a전사.이름 = "진";
+        a전사.자기소개();
+
+        a전사.a무기 = new 활();
+        a전사.공격();
+        //출력 : 진이 활로 공격합니다.
+
+        a전사.a무기 = new 칼();
+        a전사.공격();
+        //출력 : 진이 칼로 공격합니다.
     }
 }
 
-class tv{
+class 전사 {
+    // 인스턴스 변수
+    String 이름;
+    // 인스턴스 변수
+    int 나이;
+    // 인스턴스 변수
+    무기 a무기;
 
-   String lasta;
-   String lastb;
-
-    void 켜기(String a){
-        System.out.println( a + "Tv 켜집니다.");
-
-        String lasta;
+    void 자기소개() {
+        System.out.println("안녕하세요. 저는 " + this.나이 + "살 " + this.이름 + " 입니다.");
     }
-    void 끄기(String a){
-        System.out.println( a+ "Tv 꺼집니다.");
-        String lasta;
-    }
-}
 
-class 샤오미Tv extends tv{
-    void vr켜기(String a, String b ) {
-        System.out.println(a + "Tv " +  b +"켜기");
-    }
-}
-class 삼성Tv extends tv{
-    void ar켜기(String a, String b ) {
-        System.out.println(a + "Tv " + b + "켜기");
+    void 공격() {
+        a무기.공격자명 = this.이름;
+
+        a무기.작동();
+
     }
 }
 
+class 무기 {
+    String 공격자명;
 
-class LGTv extends tv{
-    void 게임모드전환(String a,String b ) {
-        System.out.println(a + "Tv " + b + "전환 ");
+    void 작동(){
+
     }
-
 }
+
+class 칼 extends 무기 {
+    void 작동(){
+        System.out.println(공격자명+"이 칼로 공격합니다.");
+    }
+}
+
+class 활 extends 무기 {
+    void 작동(){
+        System.out.println(공격자명+"이 활로 공격합니다.");
+    }
+}
+
