e9b80654962fc4f3621a37d984d3ce7bd70ae524
doomdex84 <doomdex84@gmail.com>
2025-03-14 10:14:10 +0900

    0314

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,71 +4,51 @@ package org.example;
 //            논리값 / 문자 /             정수           /      실수    /
 
 
-// 아래의 코드가 실행되도록 해주세요
-
 public class Main {
     public static void main(String[] args) {
-        전사 a전사 = new 전사();
 
-        String 이름 = "칸";
-        a전사.이름 = 이름;
-        a전사.나이 = 20;
-        a전사.자기소개();
+        Article article1 =  new Article();
+        System.out.println(article1.id);
 
-        a전사.나이++;
-        a전사.자기소개();
+        Article article2 =  new Article();
+        System.out.println(article2.id);
 
-        a전사.나이 = 30;
-        a전사.이름 = "진";
-        a전사.자기소개();
+        Article article3 =  new Article();
+        System.out.println(article3.id);
+        Article article4 =  new Article();
+        System.out.println(article4.id);
+        Article article5 =  new Article();
+        System.out.println(article5.id);
 
-        a전사.a무기 = new 활();
-        a전사.공격();
-        //출력 : 진이 활로 공격합니다.
 
-        a전사.a무기 = new 칼();
-        a전사.공격();
-        //출력 : 진이 칼로 공격합니다.
+
+
     }
 }
 
-class 전사 {
+class Article {
+    static int lastId; // 클래스 변수 , 모든 인스턴스 공유, 객체 생성과 무관
+
+    int id;
+    String regDate;
     // 인스턴스 변수
-    String 이름;
-    // 인스턴스 변수
-    int 나이;
-    // 인스턴스 변수
-    무기 a무기;
 
-    void 자기소개() {
-        System.out.println("안녕하세요. 저는 " + this.나이 + "살 " + this.이름 + " 입니다.");
+
+    static {
+        lastId = 0;
     }
 
-    void 공격() {
-        a무기.공격자명 = this.이름;
+    Article () {
+       lastId++;
+       this.id = lastId;
+    }
 
-        a무기.작동();
+    Article(int id, String regDate){
+        this.id = id;
+        this.regDate = regDate;
 
     }
+
 }
 
-class 무기 {
-    String 공격자명;
-
-    void 작동(){
-
-    }
-}
-
-class 칼 extends 무기 {
-    void 작동(){
-        System.out.println(공격자명+"이 칼로 공격합니다.");
-    }
-}
-
-class 활 extends 무기 {
-    void 작동(){
-        System.out.println(공격자명+"이 활로 공격합니다.");
-    }
-}
-
+// Article > 뜻은 게시글
